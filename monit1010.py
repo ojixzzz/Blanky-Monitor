8257e25e05d799d08a80b33ece254fb7ad1d4dbe
ojixzzz <ojixzzz@gmail.com>
2016-11-16 12:26:20 +0700

    dinamis

--- monit1010.py
+++ monit1010.py
@@ -8,6 +8,7 @@ from PyQt5.QtGui import QFont, QColor
 from PyQt5 import QtCore
 from PyQtCharts.qcharts import (PieChart, ScatterChart, LineChart, AreaChart,
                     DataTable, DialogViewer, Viewer)
+from config import api_url
 
 label1 = None
 label2 = None
@@ -91,30 +92,24 @@ class MainView(QWidget):
 
         label1.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         label1.setStyleSheet("background-color: #16a085; font-size:20px; color:#000;")
-        label1.setText("<span style='font-size:28pt;'>AutoDownload<br/> Online:<br/></span><span style='font-size:80pt;'>800</span>")
 
         label2.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         label2.setStyleSheet("background-color: #27ae60; font-size:20px; color:#000;")
-        label2.setText("<span style='font-size:28pt;'>Link4share<br/> Online:<br/></span><span style='font-size:80pt;'>600</span>")
 
         label3.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         label3.setStyleSheet("background-color: #2980b9; font-size:20px; color:#000;")
-        label3.setText("<span style='font-size:28pt;'>Apkpoke<br/> Online:<br/></span><span style='font-size:80pt;'>500</span>")
 
         label7.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         label7.setStyleSheet("background-color: #f39c12; font-size:20px; color:#000;")
-        label7.setText("<span style='font-size:28pt;'>Api 4shared:<br/></span><span style='font-size:80pt;'>Error!</span>")
 
         label8.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         label8.setStyleSheet("background-color: #d35400; font-size:20px; color:#000;")
-        label8.setText("<span style='font-size:28pt;'>Server Status:<br/></span><span style='font-size:80pt;'>OK!</span>")
 
         label9.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         label9.setStyleSheet("background-color: #c0392b; font-size:20px; color:#000;")
-        label9.setText("<span style='font-size:28pt;'>Transaksi Laundry<br/>(Today):<br/></span><span style='font-size:80pt;'>0</span>")
 
         #self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('Monit1010')  
+        self.setWindowTitle('BlankyMonit')  
         self.showFullScreen()  
         self.show()
 
@@ -135,13 +130,13 @@ class MainView(QWidget):
         chart3 = self.init_chartAnalityc('#e67e22', data['chart3'])
         grid.addWidget(chart3['chart'],1,2) 
 
-        label1.setText("<span style='font-size:28pt;'>AutoDownload<br/> Online:<br/></span><span style='font-size:80pt;'>%s</span>" % data['autodownload_online'])
-        label2.setText("<span style='font-size:28pt;'>Link4share<br/> Online:<br/></span><span style='font-size:80pt;'>%s</span>" % data['link4share_online'])
-        label3.setText("<span style='font-size:28pt;'>Apkpoke<br/> Online:<br/></span><span style='font-size:80pt;'>%s</span>" % data['apkpoke_online'])
+        label1.setText("<span style='font-size:28pt;'>%s<br/></span><span style='font-size:80pt;'>%s</span>" % (data['data1_label'], data['data1']))
+        label2.setText("<span style='font-size:28pt;'>%s<br/></span><span style='font-size:80pt;'>%s</span>" % (data['data2_label'], data['data2']))
+        label3.setText("<span style='font-size:28pt;'>%s<br/></span><span style='font-size:80pt;'>%s</span>" % (data['data3_label'], data['data3']))
 
-        label7.setText("<span style='font-size:28pt;'>Api 4shared:<br/></span><span style='font-size:80pt;'>%s</span>" % data['api4shared_status'])
-        label8.setText("<span style='font-size:28pt;'>Server Status:<br/></span><span style='font-size:80pt;'>%s</span>" % data['server_status'])
-        label9.setText("<span style='font-size:28pt;'>Transaksi Laundry<br/>(Today):<br/></span><span style='font-size:80pt;'>%s</span>" % data['laundry_today'])
+        label7.setText("<span style='font-size:28pt;'>%s<br/></span><span style='font-size:80pt;'>%s</span>" % (data['data4_label'], data['data4']))
+        label8.setText("<span style='font-size:28pt;'>%s<br/></span><span style='font-size:80pt;'>%s</span>" % (data['data5_label'], data['data5']))
+        label9.setText("<span style='font-size:28pt;'>%s<br/></span><span style='font-size:80pt;'>%s</span>" % (data['data6_label'], data['data6']))
 
 class WorkerThread(QtCore.QThread):
     updateData = QtCore.pyqtSignal(dict)
@@ -153,31 +148,40 @@ class WorkerThread(QtCore.QThread):
         data_tb = {}
         while True:
             try: 
-                req = requests.get('http://188.166.184.127:1000/data_status')
+                req = requests.get(api_url)
                 if req.status_code == 200:
                     resjson = json.loads(req.text)
                     if resjson.get('data'):
                         data_tb['chart1'] = [
-                            [ 1.00, resjson['data']['autodownload_list'][0]],
-                            [ 2.00, resjson['data']['autodownload_list'][1]],
-                            [ 3.00, resjson['data']['autodownload_list'][2]]
+                            [ 1.00, resjson['data']['chart1_list'][0]],
+                            [ 2.00, resjson['data']['chart1_list'][1]],
+                            [ 3.00, resjson['data']['chart1_list'][2]]
                         ]
                         data_tb['chart2'] = [
-                            [ 1.00, resjson['data']['link4share_list'][0]],
-                            [ 2.00, resjson['data']['link4share_list'][1]],
-                            [ 3.00, resjson['data']['link4share_list'][2]]
+                            [ 1.00, resjson['data']['chart2_list'][0]],
+                            [ 2.00, resjson['data']['chart2_list'][1]],
+                            [ 3.00, resjson['data']['chart2_list'][2]]
                         ]
                         data_tb['chart3'] = [
-                            [ 1.00, resjson['data']['apkpoke_list'][0]],
-                            [ 2.00, resjson['data']['apkpoke_list'][1]],
-                            [ 3.00, resjson['data']['apkpoke_list'][2]]
+                            [ 1.00, resjson['data']['chart3_list'][0]],
+                            [ 2.00, resjson['data']['chart3_list'][1]],
+                            [ 3.00, resjson['data']['chart3_list'][2]]
                         ]
-                        data_tb['autodownload_online'] = resjson['data']['autodownload_online']
-                        data_tb['link4share_online'] = resjson['data']['link4share_online']
-                        data_tb['apkpoke_online'] = resjson['data']['apkpoke_online']
-                        data_tb['api4shared_status'] = resjson['data']['api4shared_status']
-                        data_tb['server_status'] = resjson['data']['server_status']
-                        data_tb['laundry_today'] = 0
+
+                        data_tb['data1_label'] = resjson['data']['data1_label']
+                        data_tb['data2_label'] = resjson['data']['data2_label']
+                        data_tb['data3_label'] = resjson['data']['data3_label']
+                        data_tb['data4_label'] = resjson['data']['data4_label']
+                        data_tb['data5_label'] = resjson['data']['data5_label']
+                        data_tb['data6_label'] = resjson['data']['data6_label']
+
+                        data_tb['data1'] = resjson['data']['data1']
+                        data_tb['data2'] = resjson['data']['data2']
+                        data_tb['data3'] = resjson['data']['data3']
+                        data_tb['data4'] = resjson['data']['data4']
+                        data_tb['data5'] = resjson['data']['data5']
+                        data_tb['data6'] = resjson['data']['data6']
+
                         self.updateData.emit(data_tb)
             except Exception as e:
                 print(e)
---
+++ server_example/monitserver.py
@@ -0,0 +1,31 @@
+import falcon
+import json
+
+class DataStatusResource:
+    def on_get(self, req, resp):
+
+        monit1010 = mc.get("monit1010")
+        if monit1010:
+            data = {
+                "chart1_list": [0, 0, 0],
+                "chart2_list": [0, 0, 0],
+                "chart3_list": [0, 0, 0],
+                "data1_label": "Label 1",
+                "data2_label": "Label 2",
+                "data3_label": "Label 3",
+                "data4_label": "Label 4",
+                "data5_label": "Label 5",
+                "data6_label": "Label 6",
+                "data1": 0,
+                "data2": 0,
+                "data3": 0,
+                "data4": 0,
+                "data5": 0,
+                "data6": "Ok",
+            }
+            resp.body = json.dumps({"status": 200, "data": monit1010})
+        else:
+            resp.body = json.dumps({"status": 404})
+
+api = falcon.API()
+api.add_route('/data_status', DataStatusResource())
